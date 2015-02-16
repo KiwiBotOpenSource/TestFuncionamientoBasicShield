d63a6ec0303163cefe0e1b1ec3fe85733983e479
alberto <alb.pum96@gmail.com>
2015-02-16 13:45:39 +0100

    Aumentado el tiempo de cambio de color del LED RGB

--- TestFuncionamientoBasicShield.ino
+++ TestFuncionamientoBasicShield.ino
@@ -4,13 +4,12 @@ byte j[7] = {
   B1000000, B0100000, B0010000, B0001000, B0000100, B0000010, B0000001};
 int iMatriz;
 int jMatriz;
-
 boolean estado=0;
-
 int pinLatch=6;
 int pinReloj=9;
 int pinDatos=5;
-
+//millis
+long tiempoIni=0, tiempoFin;
 //SWITCHS
 const int switch1Pin = 2;
 const int switch2Pin = 3;
@@ -46,25 +45,21 @@ int yacelValue=0;
 int trigPin=11;
 int echoPin=12;
 int distancia;
-
 String zumbador;
 String rgb;
 String ldr;
 String acelerometro;
 String distance;
 String lm35;
-
 void setup() {
   attachInterrupt(0, salir ,RISING);
   // initialize the LED pin as an output:
   pinMode(ledPinR, OUTPUT);
   pinMode(ledPinG, OUTPUT);
   pinMode(ledPinB, OUTPUT);
-
   pinMode(pinLatch, OUTPUT);
   pinMode(pinDatos, OUTPUT);
   pinMode(pinReloj, OUTPUT);
-
   pinMode(buzzerPin, OUTPUT);
   // initialize the pushbutton pin as an input:
   pinMode(switch1Pin, INPUT);
@@ -73,16 +68,17 @@ void setup() {
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
   Serial.begin(9600);
-
   ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   Serial.println("======Probando el LED RGB======");
   Serial.println("ASEGURESE DE TENER EL CONMUTADOR EN LA POSICION DEL LED RGB");
   Serial.println("Si puede ver la sucesion de colores(Rojo, Azul, Verde) pulse el boton derecho.");
-  Serial.println("      De no ser asi, pulse el boton izquierdo");
+  Serial.println(" De no ser asi, pulse el boton izquierdo");
   while(switch2State == LOW && switch1State == LOW){
     digitalWrite(ledPinG, LOW);
     digitalWrite(ledPinR, HIGH);
-    for(int i=0; i<30000; i++){
+    tiempoIni=millis();
+    while(tiempoFin-tiempoIni<500){
+      tiempoFin=millis();
       switch2State=digitalRead(switch2Pin);
       if(switch2State == HIGH || switch1State == HIGH){
         break;
@@ -93,7 +89,9 @@ void setup() {
     }
     digitalWrite(ledPinR, LOW);
     digitalWrite(ledPinB, HIGH);
-    for(int i=0; i<30000; i++){
+    tiempoIni=millis();
+    while(tiempoFin-tiempoIni<500){
+      tiempoFin=millis();
       switch2State=digitalRead(switch2Pin);
       if(switch2State == HIGH || switch1State == HIGH){
         break;
@@ -104,7 +102,9 @@ void setup() {
     }
     digitalWrite(ledPinB, LOW);
     digitalWrite(ledPinG, HIGH);
-    for(int i=0; i<30000; i++){
+    tiempoIni=millis();
+    while(tiempoFin-tiempoIni<500){
+      tiempoFin=millis();
       switch2State=digitalRead(switch2Pin);
       if(switch2State == HIGH || switch1State == HIGH){
         break;
@@ -123,19 +123,17 @@ void setup() {
   else{
     rgb ="#El LED RGB funciona correctamente";
   }
-
   while(switch2State == HIGH || switch1State == HIGH){
     switch1State=digitalRead(switch1Pin);
     switch2State=digitalRead(switch2Pin);
   }
-
   ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   Serial.println("======Probando el zumbador======");
   digitalWrite(buzzerPin, HIGH);
   delay(500);
   digitalWrite(buzzerPin, LOW);
   Serial.println("Si ha escuchado el pitido pulse el boton derecho.");
-  Serial.println("      De no ser asi, pulse el boton izquierdo");
+  Serial.println(" De no ser asi, pulse el boton izquierdo");
   while(switch2State == LOW && switch1State == LOW){
     switch2State=digitalRead(switch2Pin);
   }
@@ -145,12 +143,10 @@ void setup() {
   else{
     zumbador ="#El zumbador funciona correctamente";
   }
-
   while(switch2State == HIGH || switch1State == HIGH){
     switch1State=digitalRead(switch1Pin);
     switch2State=digitalRead(switch2Pin);
   }
-
   ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   Serial.println("======Probando la LDR======");
   valorLDRini=analogRead(LDRPin);
@@ -167,19 +163,17 @@ void setup() {
   else{
     ldr ="#El LDR funciona correctamente";
   }
-
   while(switch1State == HIGH){
     switch1State=digitalRead(switch1Pin);
   }
-
   ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   Serial.println("======Probando la SENSOR DISTANCIA======");
   Serial.println("Mida la distancia del ancho de un folio A4");
   Serial.println("Si no se detecta cambio en el sensor, pulse el boton izquierdo");
   while((distancia < 17 || distancia > 23 )&& switch1State == LOW){
-    digitalWrite(trigPin,LOW); 
+    digitalWrite(trigPin,LOW);
     delayMicroseconds(5);
-    digitalWrite(trigPin, HIGH); 
+    digitalWrite(trigPin, HIGH);
     delayMicroseconds(10);
     distancia=pulseIn(echoPin, HIGH);
     distancia = (0.017*distancia);
@@ -193,7 +187,6 @@ void setup() {
   while(switch1State == HIGH){
     switch1State=digitalRead(switch1Pin);
   }
-
   //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   Serial.println("======Probando la LM35======");
   Serial.println("Suba la tempertura del sensor");
@@ -210,7 +203,6 @@ void setup() {
   else{
     lm35 ="#El LM35 funciona correctamente";
   }
-
   while(switch1State == HIGH){
     switch1State=digitalRead(switch1Pin);
   }
@@ -225,12 +217,11 @@ void setup() {
   Serial.println(distance);
   delay(10);
   Serial.println(lm35);
-
+  Serial.println("");
   Serial.println("======Por ultimo comprueba que el punto en la matriz se mueve acuerdo al joystic o acelerometro======");
   Serial.println("ASEGURESE DE TENER EL CONMUTADOR EN LA POSICION DE LA MATRIZ DE LED");
   Serial.println("Para cambiar entre acelerometro y joystic usa el boton izquierdo");
 }
-
 void loop(){
   if(switch1State==HIGH){
     estado=!estado;
@@ -249,21 +240,18 @@ void loop(){
     iMatriz=((1024-xjoyValue)/160);
     jMatriz=(yjoyValue/120);
   }
-
   if(estado==1){
     xacelValue=analogRead(xacelPin);
     yacelValue=analogRead(yacelPin);
     iMatriz=((yacelValue-210)/40);
     jMatriz=((300-(xacelValue-210))/42);
   }
-
   if(iMatriz>5){
     iMatriz=5;
   }
   if(iMatriz<2){
     iMatriz=1;
   }
-
   if(jMatriz>7){
     jMatriz=7;
   }
@@ -275,31 +263,7 @@ void loop(){
   shiftOut(pinDatos, pinReloj, MSBFIRST, j[jMatriz-1]);
   digitalWrite(pinLatch, HIGH);
 }
-
 void salir(){
   switch1State=HIGH;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
